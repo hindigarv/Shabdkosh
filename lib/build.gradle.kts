d003ad640c5cdcaf6bee18ad1bf3e9a261285ce1
hindigarv <hindi.garv.001@gmail.com>
2022-06-18 19:06:42 +0200

    add configuration to publish to maven central repo

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -9,6 +9,69 @@
 plugins {
     // Apply the java-library plugin for API and implementation separation.
     `java-library`
+    `maven-publish`
+    signing
+}
+
+group = "io.github.hindigarv"
+version = "0.1.0"
+
+java {
+    withJavadocJar()
+    withSourcesJar()
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            groupId = "io.github.hindigarv"
+            artifactId = "shabdkosh"
+            version = "0.1.0"
+            from(components["java"])
+            pom {
+                name.set("shabdkosh")
+                description.set("A HindiGarv Shabdkosh")
+                url.set("https://github.com/hindigarv/Shabdkosh")
+                inceptionYear.set("2022")
+                properties.set(mapOf(
+                    "myProp" to "value",
+                    "prop.with.dots" to "anotherValue"
+                ))
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("hindigarv")
+                        name.set("Hindi Garv")
+                        email.set("hindi.garv.001@gmail.com")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:git://github.com/hindigarv/Shabdkosh.git")
+                    developerConnection.set("scm:git:ssh://github.com/hindigarv/Shabdkosh.git")
+                    url.set("https://github.com/hindigarv/Shabdkosh")
+                }
+            }
+        }
+    }
+    repositories {
+        maven {
+            name = "ossrh"
+            url = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2")
+            credentials {
+                username = project.property("ossrh_username") as String
+                password = project.property("ossrh_password") as String
+            }
+        }
+    }
+}
+
+signing {
+    sign(publishing.publications["mavenJava"])
 }
 
 repositories {
