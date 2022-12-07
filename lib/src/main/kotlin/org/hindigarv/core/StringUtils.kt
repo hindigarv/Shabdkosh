6fb03c938d26906b891f036a0f450d5d51045c9d
hindigarv <hindi.garv.001@gmail.com>
2022-12-07 14:34:52 +0100

    change some methods to expression body

--- lib/src/main/kotlin/org/hindigarv/core/StringUtils.kt
+++ lib/src/main/kotlin/org/hindigarv/core/StringUtils.kt
@@ -5,19 +5,15 @@ val devanagariChars =
         "ऀँंःऄअआइईउऊऋऌऍऎएऐऑऒओऔकखगघङचछजझञटठडढणतथदधनऩपफबभमयरऱलळऴवशषसहऺऻ़ऽािीुूृॄॅॆेैॉॊोौ्ॎॏॐ॒॑॓॔ॕॖॗक़ख़ग़ज़ड़ढ़फ़य़ॠॡॢॣॱॲॳॴॵॶॷॸॹॺॻॼॽॾॿ"
 
 
-fun String.tokenize(): List<String> {
-    return EmojiParser.removeAllEmojis(this)
-        .split(Regex("[^${devanagariChars}]"))
-        .filter { it.isNotBlank() }
-}
+fun String.tokenize(): List<String> = this
+    .split(Regex("[^${devanagariChars}]"))
+    .filter { it.isNotBlank() }
 
-fun String.removeNukta(): String {
-    return this.replace('क़', 'क')
-        .replace('ख़', 'ख')
-        .replace('ग़', 'ग')
-        .replace('ज़', 'ज')
-        .replace('ड़', 'ड')
-        .replace('ढ़', 'ढ')
-        .replace('फ़', 'फ')
-        .replace("़", "")
-}
+fun String.removeNukta(): String = this.replace('क़', 'क')
+    .replace('ख़', 'ख')
+    .replace('ग़', 'ग')
+    .replace('ज़', 'ज')
+    .replace('ड़', 'ड')
+    .replace('ढ़', 'ढ')
+    .replace('फ़', 'फ')
+    .replace("़", "")
